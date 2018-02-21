052e4e8f67446620a26cf661bb47a300b3c4f30c
Laur (personal) <laurpersonal@Laurs-MacBook-Pro.local>
2018-02-21 10:54:02 +0200

    attempt at creating file 2

--- wsgi.py
+++ wsgi.py
@@ -9,12 +9,16 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    dir = "/mnt/"
+    dir = "./mnt/"
     fn = dir + "log.txt"
 
+    if os.path.exists(fn):
+        append_write = 'a'
+    else:
+        append_write = 'w'
 
-    file = open(fn, 'w')
-    file.write("Hostname: " + str(socket.gethostname()) + " Timestamp: " + str(time.time()))
+    file = open(fn, append_write)
+    file.write("Hostname: " + str(socket.gethostname()) + " Timestamp: " + str(time.time()) + "<br>")
     file.close()
 
     file = open(fn, 'r')
