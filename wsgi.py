714d0cf77635010e664cb0d88c7fd336a860f95e
Laur (personal) <laurpersonal@Laurs-MacBook-Pro.local>
2018-02-21 10:18:15 +0200

    testing folder

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,12 @@
 import socket
+import os
 from flask import Flask
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    
+    os.listdir("/mnt/")
     return "Hello World! Greetings from "+socket.gethostname()+"\n"
 
 
